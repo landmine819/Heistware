e4a313b2c97a1ba754b856e1dd405a2afe40184e
landmine819 <wdk990@yahoo.com>
2025-08-18 12:47:59 -0400

    Heistware (BETA) v1

---
+++ Initiate.lua
@@ -0,0 +1,13 @@
+if game.PlaceId == 15784744207 then
+    loadfile('Valley Prison.lua')()
+elseif game.PlaceId == 79546208627805 then
+    loadfile('99 Nights in the Forest')()
+elseif game.PlaceId == 115286378269814 then
+    loadfile('Protect The House From Monsters')()
+elseif game.PlaceId == 123357112612493 then
+    loadfile('Shovel Spleef')()
+elseif game.PlaceId == 129827112113663 then
+    loadfile('prospecting.lua')()
+else 
+    loadfile('Universal.lua')()
+end
